19b0efb4795573781c237848dea0c3cc176ce7b9
中北竜馬 <b1020183@fun.ac.jp>
2024-03-20 22:33:33 +0900

    [Sleep] SleepWidgetのテスト書いた
    Widgetテストのやり方をだいたい理解した

--- test/feature/sleep/widget/sleep_test.dart
+++ test/feature/sleep/widget/sleep_test.dart
@@ -1,61 +1,111 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:routine_builder/feature/sleep/widget/sleep.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mockito/mockito.dart';
-import 'package:mockito/annotations.dart';
 import 'package:routine_builder/feature/sleep/class/sleep_controller.dart';
 import 'package:routine_builder/general/enum/statuses.dart';
 import 'package:routine_builder/general/provider/user_provider.dart';
+import '../../../general/query/client/sleep_query_client_test.mocks.dart';
+import "package:routine_builder/feature/sleep/hook/use_sleep.dart";
+import 'package:flutter_hooks_test/flutter_hooks_test.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:routine_builder/general/class/user.dart';
 
-@GenerateNiceMocks([MockSpec<SleepController>(as: #MockSleepController)])
-import 'sleep_test.mocks.dart';
+final sleepQueryClient = MockSleepQueryClient();
 
 void main() {
-  testWidgets('Sleep widget test', (tester) async {
-    final controller = MockSleepController();
+  tearDown(() {
+    reset(sleepQueryClient);
+  });
 
-    await tester.pumpWidget(
-        ProviderScope(child: TestSleepWidget(controller: controller)));
+  group("Sleep Widget", () {
+    testWidgets('When sleep button is tapped.', (tester) async {
+      await draw(tester, Statuses.none);
 
-    Finder sleepButton;
-    Finder wakeUpButton;
-    
-    //「寝る」ボタンが表示されていて、「起きる」ボタンが表示されていない
-    sleepButton = find.byKey(const Key("sleep_button"));
-    wakeUpButton = find.byKey(const Key("wake_up_button"));
-    expect(sleepButton, findsOneWidget);
-    expect(wakeUpButton, findsNothing);
-    
-    //「寝る」ボタンを押すと、controllerのhandleSleepが1回呼ばれる
-    await tester.tap(sleepButton);
+      // 「寝る」と「昼寝」ボタンが表示されていて、「起きる」ボタンが表示されていない
+      var sleepButton = find.byKey(const Key("sleep_button"));
+      var napButton = find.byKey(const Key("nap_button"));
+      var wakeUpButton = find.byKey(const Key("wake_up_button"));
+      expect(sleepButton, findsOneWidget);
+      expect(napButton, findsOneWidget);
+      expect(wakeUpButton, findsNothing);
+
+      // 寝るボタンをタップすると、sleepQueryClient.startSleepが1回呼ばれる
+      await tester.tap(sleepButton);
+      await tester.pump();
+      verify(sleepQueryClient.startSleep()).called(1);
+
+      // 起きるボタンが表示されていて、「寝る」と「昼寝」ボタンが表示されていない
+      sleepButton = find.byKey(const Key("sleep_button"));
+      napButton = find.byKey(const Key("nap_button"));
+      wakeUpButton = find.byKey(const Key("wake_up_button"));
+      expect(sleepButton, findsNothing);
+      expect(napButton, findsNothing);
+      expect(wakeUpButton, findsOneWidget);
+    });
+  });
+
+  testWidgets("When nap button is tapped.", (tester) async {
+    await draw(tester, Statuses.none);
+
+    //「昼寝」ボタンをタップすると、sleepQueryClient.startSleepが1回呼ばれる
+    var napButton = find.byKey(const Key("nap_button"));
+    await tester.tap(napButton);
     await tester.pump();
-    verify(controller.handleSleep()).called(1);
+    verify(sleepQueryClient.startSleep(isNap: true)).called(1);
 
-    //「寝る」ボタンが表示されていなくて、「起きる」ボタンが表示されている
-    sleepButton = find.byKey(const Key("sleep_button"));
-    wakeUpButton = find.byKey(const Key("wake_up_button"));
+    // 起きるボタンが表示されていて、「寝る」と「昼寝」ボタンが表示されていない
+    napButton = find.byKey(const Key("nap_button"));
+    var sleepButton = find.byKey(const Key("sleep_button"));
+    var wakeUpButton = find.byKey(const Key("wake_up_button"));
+    expect(napButton, findsNothing);
     expect(sleepButton, findsNothing);
     expect(wakeUpButton, findsOneWidget);
+  });
 
-    //「起きる」ボタンを押すと、controllerのhandleWakeUpが1回呼ばれる
-    wakeUpButton = find.byKey(const Key("wake_up_button"));
+  testWidgets("When wake up button is tapped.", (tester) async {
+    await draw(tester, Statuses.sleeping);
+
+    //「起きる」ボタンをタップすると、sleepQueryClient.finishSleepが1回呼ばれる
+    var wakeUpButton = find.byKey(const Key("wake_up_button"));
     await tester.tap(wakeUpButton);
     await tester.pump();
-    verify(controller.handleWakeUp()).called(1);
+    verify(sleepQueryClient.finishSleep()).called(1);
+
+    //「寝る」と「昼寝」ボタンが表示されていて、「起きる」ボタンが表示されていない
+    var sleepButton = find.byKey(const Key("sleep_button"));
+    var napButton = find.byKey(const Key("nap_button"));
+    wakeUpButton = find.byKey(const Key("wake_up_button"));
+    expect(sleepButton, findsOneWidget);
+    expect(napButton, findsOneWidget);
+    expect(wakeUpButton, findsNothing);
   });
 }
 
-class TestSleepWidget extends ConsumerWidget {
-  final MockSleepController controller;
-  const TestSleepWidget({super.key, required this.controller});
+Future<void> draw(WidgetTester tester, Statuses initialStatus) async {
+  final container = ProviderContainer(overrides: [
+    userProvider.overrideWith(
+        (ref) => UserNotifier(User(name: "test", status: initialStatus))),
+  ]);
+  final result = await buildHook((_) {
+    return useSleep(container.read(userProvider.notifier),
+        sleepQueryClient: sleepQueryClient);
+  });
+  final SleepController controller = result.current;
+
+  await tester.pumpWidget(
+    UncontrolledProviderScope(
+        container: container, child: TestSleep(controller: controller)),
+  );
+}
+
+class TestSleep extends HookConsumerWidget {
+  final SleepController controller;
+  const TestSleep({super.key, required this.controller});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    when(controller.handleSleep()).thenAnswer((_) => () {
-          final notifier = ref.read(userProvider.notifier);
-          notifier.updateStatus(Statuses.sleeping);
-        });
     return MaterialApp(
       home: Sleep(controller: controller),
     );
