4461f0e321022a566b6351a6d2edd369b773021e
中北竜馬 <b1020183@fun.ac.jp>
2024-03-30 23:01:33 +0900

    [Tooth] HomeにToothButton置いた

---
+++ lib/feature/tooth/widget/tooth.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class Tooth extends StatelessWidget {
+  Tooth({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Tooth"),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
-  await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: ".env.development");
   runApp(const ProviderScope(child: App()));
 }
